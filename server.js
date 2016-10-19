0bd78589574aa8b3b9ec87d8b8aab541cc89dce2
somasundarkr <somasundar.kr@gmail.com>
2016-10-19 16:00:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,40 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var articles = {
+    'article-one':{
+                    title : 'Article One| SomasundarKr',
+                    heading: 'Article One',
+                    date:'10/10/2016',
+                    content:'<p> This is the content of the first page of the Article </p>'
+    },
+    'article-two':{
+                    title : 'Article Two| SomasundarKr',
+                    heading: 'Article Two',
+                    date:'15/10/2016',
+                    content:'<p> This is the content for My Second Article </p>'
+    },
+    'article-Three':{
+                    title : 'Article One| SomasundarKr',
+                    heading: 'Article Three',
+                    date:'19/10/2016',
+                    content:'<p> This is the content for My Third Webapp Article </p>'
+    }
+};
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+        
+        var htmlTemplate = 
+                            '<html><head><title> ${title} </title><meta name = "viewport" content = "width=device-width, initial-scale = 1/><link href = "/ui/style.css" rel = "stylesheet" /></head><body><div class = "container"><div><a href = "/">Home</a></div><hr/><h3> ${heading} </h3><div>${date}</div></div></body></html>';
+        return htmlTemplate;    
+}
+app.get('/:articleName', function (req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
