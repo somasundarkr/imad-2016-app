b9252a89447b7d76832d40522421baee38089950
somasundarkr <somasundar.kr@gmail.com>
2016-10-26 12:45:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,11 @@ var htmlTemplate =`
                             <title> 
                                     ${title} 
                             </title>
-                            <meta name = "viewport" content = "width=device-width, initial-scale = 1 />
+                            <meta charset="utf-8">
+                             <meta name="viewport" content="width=device-width, initial-scale=1" />
+                              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                              <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
                             <link href="/ui/style.css" rel="stylesheet" />
                             </head>
                             <body>
