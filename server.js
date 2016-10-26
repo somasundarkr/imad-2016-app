c9c61126232aa3828d3c0daf8fda38fb8ff1fa3a
somasundarkr <somasundar.kr@gmail.com>
2016-10-26 22:16:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,14 +57,30 @@ var htmlTemplate =`
                                 <div>${date}</div>
                             <hr/>
                                 <div> ${content} </div>
-                                <label>Enter comments below</label></br>
-                                 <textarea name='comment' id='comment'></textarea><br />
-                                  <input type="submit" id="comment_btn" value="Submit" class="btn btn-warning"></input>
-                                  <hr>
-                                  <p>Comments :<br>
-                                    <span id="comments"></span>
-                                  </p>
-            </div>
+                                <!-- Container (Contact Section) -->
+                                    <div id="contact" class="container">
+                                      <h3 class="text-center">Contact</h3>
+                                      <p class="text-center"><em>We value your feed back!</em></p>
+                                         	<div class="col-md-12">
+                                          		<div class="row">
+                                            			<div class="col-sm-6 form-group">
+                                    <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                                           			 </div>
+                                           			 <div class="col-sm-6 form-group">
+                                    <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                                            			</div>
+                                         		 </div>
+                                     <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
+                                          		<br>
+                                          		<div class="row">
+                                            			<div class="col-md-12 form-group">
+                                              				<button class="btn pull-right" type="submit">Send</button>
+                                            			</div>
+                                          		</div>
+                                        	</div>
+                                      </div>
+                                      <br>	
+                </div>
                             </body>
 </html>
                             `;
