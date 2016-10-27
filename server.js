0b7a0bd0f08e5a883b82b9907019cdd055c06f84
somasundarkr <somasundar.kr@gmail.com>
2016-10-27 13:34:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ var htmlTemplate =`
                                             			<div class="col-sm-6 form-group">
                                     <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
                                            			 </div>
-                                           			 <div class="col-sm-6 form-group">
+                                           			 /*<div class="col-sm-6 form-group">
                                     <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-                                            			</div>
+                                            			</div> */
                                          		 </div>
                                      <textarea class="form-control" id="submit_btn" name="comments" placeholder="Comment" rows="5"></textarea>
                                           		<br>
