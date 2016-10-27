60ccd0de6b3904d3368a63fefa7946000b539e25
somasundarkr <somasundar.kr@gmail.com>
2016-10-27 12:31:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,39 +57,29 @@ var htmlTemplate =`
                                 <div>${date}</div>
                             <hr/>
                                 <div> ${content} </div>
-                               // send form data with JavaScript
-                                if( window.FormData) {
-                                
-                                	var appendComment = function (nameValue, commentValue) {
-                                		var comment = document.createElement('li');
-                                		var commentName = document.createElement('h4');
-                                		var commentComment = document.createElement('p');
-                                		var commentWrapper = document.querySelector('.comments');
-                                		commentName.innerText = nameValue;
-                                		commentComment.innerText = commentValue;
-                                		nameValue ? comment.appendChild(commentName) : '';
-                                		comment.appendChild(commentComment);
-                                		commentWrapper.appendChild(comment);
-                                	};
-                                
-                                	form.addEventListener('submit', function (ev) {
-                                		var formData = new FormData(form);
-                                		commentValue = commentArea.value;
-                                		nameValue = nameInput.value;
-                                
-                                		var xhr = new XMLHttpRequest();
-                                		// save the comment in the database
-                                		xhr.open('POST', './save', true);
-                                		xhr.onload = function () {
-                                			appendComment(nameValue, commentValue);
-                                		};
-                                		xhr.send(formData);
-                                
-                                		// always call preventDefault at the end, see: http://molily.de/javascript-failure/
-                                		ev.preventDefault();
-                                	});
-                                }
-                                  <br>	
+                                <!-- Container (Contact Section) -->
+                                    <div id="contact" class="container">
+                                      <h3 class="text-center">Contact</h3>
+                                      <p class="text-center"><em>We value your feed back!</em></p>
+                                         	<div class="col-md-12">
+                                          		<div class="row">
+                                            			<div class="col-sm-6 form-group">
+                                    <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                                           			 </div>
+                                           			 <div class="col-sm-6 form-group">
+                                    <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                                            			</div>
+                                         		 </div>
+                                     <textarea class="form-control" id="submit_btn" name="comments" placeholder="Comment" rows="5"></textarea>
+                                          		<br>
+                                          		<div class="row">
+                                            			<div class="col-md-12 form-group">
+                                              				<button class="btn pull-right" type="submit">Send</button>
+                                            			</div>
+                                          		</div>
+                                        	</div>
+                                      </div>
+                                      <br>	
                 </div>
                             </body>
 </html>
