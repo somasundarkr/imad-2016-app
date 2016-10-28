bdcbd98e147c6b601fdf35f5e16e0edf61ac4aab
somasundarkr <somasundar.kr@gmail.com>
2016-10-28 16:18:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ function createTemplate (data) {
               ${title}
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
           <link href="/ui/style.css" rel="stylesheet" />
       </head> 
       <body>
