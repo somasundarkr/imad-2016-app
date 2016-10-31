ddca1ad087d30c64e9361c3ca9313587b015fc30
somasundarkr <somasundar.kr@gmail.com>
2016-10-31 16:47:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,30 +101,7 @@ app.get('/articles/:articleName', function (req, res) {
         }
     }
   });
-  pool.query("SELECT * FROM article WHERE title = $2", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-    }
-  });
-  pool.query("SELECT * FROM article WHERE title = $3", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-    }
-  });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
