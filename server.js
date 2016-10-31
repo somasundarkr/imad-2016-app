1092fdbe53db39574750542839ba50d2155332e3
somasundarkr <somasundar.kr@gmail.com>
2016-10-31 16:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
 
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM article WHERE title = $1 and $2 and $3", [req.params.articleName], function (err, result) {
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
     } else {
@@ -101,7 +101,30 @@ app.get('/articles/:articleName', function (req, res) {
         }
     }
   });
-  
+  pool.query("SELECT * FROM article WHERE title = $2", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+  pool.query("SELECT * FROM article WHERE title = $3", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
