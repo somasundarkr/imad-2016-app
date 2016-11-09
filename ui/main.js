abfc4e3e612ca8c8b7b57eee1f31f54132695947
somasundarkr <somasundar.kr@gmail.com>
2016-11-09 16:32:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,22 @@
     var commentBtn = document.getElementById('comment_btn');
-commentBtn.onclick = function (){
-   var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    console.log('comment is : ',comment);
-    
+commentBtn.onclick = function () {
+   
+  //create a request object  
     var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
+// capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
             if(request.status === 200){
+                //capture the list comments and it as a list
                 var comments = request.responseText;
                 console.log('comments is : ',comments);
                 comments = JSON.parse(comments);
                 console.log('comments is : ',comments);
-                var comment_list='';
-                for(var i=0;i<comments.length;i++){
+                var comment_list = '';
+                for(var i=0; i< comments.length; i++){
                     var t=i+1;
-                    comment_list+='<p>'+'comment '+t+': '+comments[i]+'</p>'+ '<hr>' ;
+                    comment_list += '<p>' + 'comment '+t+': '+comments[i]+'</p>'+ '<hr>' ;
                 }
                 var commentz = document.getElementById('comments');
                 commentz.innerHTML = comment_list;
@@ -25,6 +25,10 @@ commentBtn.onclick = function (){
         }
 
     };
+    //Make the  request
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    console.log('comment is : ',comment);
    request.open('GET', 'http://somasundarkr.imad.hasura-app.io/submit_comment?comment=' +comment, true);
    request.send(null);
 
