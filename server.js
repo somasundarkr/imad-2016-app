fbf159af253d50e83af3fed1ea1eec8989f5220e
somasundarkr <somasundar.kr@gmail.com>
2016-11-10 18:00:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create-user', function (req, res) {
    // username, password
-   // {"username": "tanmai", "password": "password"}
+   // {"username": "somasundarkr", "password": "password"}
    // JSON
    var username = req.body.username;
    var password = req.body.password;
