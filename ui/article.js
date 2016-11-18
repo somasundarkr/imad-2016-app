cf4ab52c71636480987dccd1b23c12c22d404f68
somasundarkr <somasundar.kr@gmail.com>
2016-11-18 17:04:38 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,28 +1,22 @@
-// Eg: somasundarkr.imad.hasura-app.io/articles/Article-One will result in Article-One
-
 var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
-    
     var commentFormHtml = `
-                            <h5>Submit a comment</h5>
-                            <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-                            <br/>
-                            <input type="submit" id="submit" value="Submit" />
-                            <br/>`;
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
-    
     var submit = document.getElementById('submit');
     submit.onclick = function () {
-    
         // Create a request object
-    
         var request = new XMLHttpRequest();
         
         // Capture the response and store it in a variable
-    
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
@@ -38,7 +32,6 @@ function loadCommentForm () {
         };
         
         // Make the request
-    
         var comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
@@ -47,8 +40,6 @@ function loadCommentForm () {
         
     };
 }
-
-
 
 function loadLogin () {
     // Check if the user is already logged in
@@ -65,8 +56,6 @@ function loadLogin () {
     request.send(null);
 }
 
-
-
 function escapeHTML (text)
 {
     var $text = document.createTextNode(text);
@@ -75,22 +64,15 @@ function escapeHTML (text)
     return $div.innerHTML;
 }
 
-
-
 function loadComments () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        
         if (request.readyState === XMLHttpRequest.DONE) {
-        
             var comments = document.getElementById('comments');
-        
             if (request.status === 200) {
-        
                 var content = '';
                 var commentsData = JSON.parse(this.responseText);
-        
                 for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
                     content += `<div class="comment">
@@ -100,11 +82,9 @@ function loadComments () {
                         </div>
                     </div>`;
                 }
-        
                 comments.innerHTML = content;
-        
             } else {
-                comments = document.getElementById("Oops! Could not load comments!").innerHTML;
+                comments.innerHTML('Oops! Could not load comments!');
             }
         }
     };
@@ -112,7 +92,6 @@ function loadComments () {
     request.open('GET', '/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
-
 
 
 // The first thing to do is to check if the user is logged in!
