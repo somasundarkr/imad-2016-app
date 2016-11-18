dfb26188ef09e9ca06404ab799b013773c97429e
somasundarkr <somasundar.kr@gmail.com>
2016-11-18 18:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ function createTemplate (data) {
                             <div class = para>${content}</div>
                             <hr/>
                             <h4>Comments</h4>
+                            <h5>Submit a comment</h5>
+                            <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+                            <br/>
+                            <input type="submit" id="submit" value="Submit" />
+                            <br/>
                             <div id="comment_form"></div>
                             <div id="comments">
                             <center>Loading comments...</center>
