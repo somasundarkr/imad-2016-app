902b6cada3664b71f5635558d9c9b5a1c21d04c8
somasundarkr <somasundar.kr@gmail.com>
2016-11-23 04:18:34 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -89,7 +89,7 @@ function loadComments () {
         }
     };
     
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.open('GET', 'http://somasundarkr.imad.hasura-app.io/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
 
