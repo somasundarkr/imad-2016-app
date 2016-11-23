6e34ccc77041bb78417a12705e4ff12080b5c190
somasundarkr <somasundar.kr@gmail.com>
2016-11-23 07:36:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,12 +36,19 @@ function createTemplate (data) {
             <title>${title}</title>
                     
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+            <link href="/ui/style.css" rel="stylesheet">
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <!-- Latest compiled and minified CSS -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <!-- jQuery library -->
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <!-- Latest compiled JavaScript -->
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <!-- Fontawesome library -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
             
             <script type = " text/javascript " src = "ui/article.js"></script>
-            <link href="/ui/style.css" rel="stylesheet" />
             </head>
                     <body>
                         <div class="container">
