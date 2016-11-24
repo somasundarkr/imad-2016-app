0a79c3c03a43c58907626fe88e34cea122340a54
somasundarkr <somasundar.kr@gmail.com>
2016-11-24 10:57:33 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -98,7 +98,7 @@ function loadComments () {
                 }
                 comments.innerHTML = content;
             } else {
-                comments.innerHTML('Oops! Could not load comments!');
+                comments.innerHTML = 'Oops! Could not load comments!';
             }
         }
     };
