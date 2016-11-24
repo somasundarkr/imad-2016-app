c95dd08c83b9728387edc7dae91d464067414355
somasundarkr <somasundar.kr@gmail.com>
2016-11-24 10:27:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/articles/article.js', function (req, res) {
+app.get('/ui/article.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.js'));
 });
 
