51cce24931b86b3b6618b7a032fde342e897855f
somasundarkr <somasundar.kr@gmail.com>
2016-11-24 10:25:30 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -37,7 +37,7 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
-        request.open('POST', 'http://somasundarkr.imad.hasura-app.io/submit-comment/' + currentArticleTitle, true);
+        request.open('POST', 'http://somasundarkr.imad.hasura-app.io/submit-comment/:articleName' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
