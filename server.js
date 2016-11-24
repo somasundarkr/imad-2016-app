85c51def88e724593ad5eb7e45cbeeb1330c350e
somasundarkr <somasundar.kr@gmail.com>
2016-11-24 11:05:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ function createTemplate (data) {
                             <div id="comment_form">
                             
                             </div>
-                            <div id="comments">
+                            <div id="Comments">
                             
                             <center>Loading comments....</center>
                             
