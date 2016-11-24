14adfd9069180e4cfdcf5ff46016970572f1d13e
somasundarkr <somasundar.kr@gmail.com>
2016-11-24 13:22:57 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -58,7 +58,7 @@ function loadLogin () {
         }
     };
     
-    request.open('GET', 'http://somasundarkr.imad.hasura-app.io/check-login', true);
+    request.open('GET', '/check-login', true);
     request.send(null);
 }
 
