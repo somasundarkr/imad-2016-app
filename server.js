9ac8e36ab60664b9d27d1d69fe5bf04b009a74d1
somasundarkr <somasundar.kr@gmail.com>
2016-11-30 14:55:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ function createTemplate (data) {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="/ui/style.css" rel="stylesheet">
           <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1">
           <!-- Latest compiled and minified CSS -->
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
